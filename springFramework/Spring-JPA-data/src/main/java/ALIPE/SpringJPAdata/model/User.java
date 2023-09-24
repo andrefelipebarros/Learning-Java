df39503e2572b658ef7e17434b86dfcb0cdb6b02
André Felipe de Barros A. N <afbarrosan@gmail.com>
2023-09-24 15:36:00 -0300

    🐘 PostgreSQL in the project

--- springFramework/Spring-JPA-data/src/main/java/ALIPE/SpringJPAdata/model/User.java
+++ springFramework/Spring-JPA-data/src/main/java/ALIPE/SpringJPAdata/model/User.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tab_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
