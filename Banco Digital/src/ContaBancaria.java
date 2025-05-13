1084c1800b51e5fd01974566f50df94d8c36fed4
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-05-13 10:49:32 -0300

    feat: Create ContaBancaria.java

---
+++ Banco Digital/src/ContaBancaria.java
@@ -0,0 +1,51 @@
+public abstract class ContaBancaria implements Conta {
+
+    private static int AGENCIA_PADRAO = 0; 
+    private static int SEQUENCIAL = 1; 
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public ContaBancaria(Cliente cliente) {
+        this.agencia = ContaBancaria.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++; 
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    @Override
+    public void transferir (double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void imprimirInfosComuns(){
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Conta: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+}
